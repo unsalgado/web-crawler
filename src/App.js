6f9386efc05aa4ffbd9ad9ee7e414e6eb7e14344
udayas <udayas@cclk.lk>
2022-06-30 00:53:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Stack from '@mui/material/Stack';
+import "./App.css";
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import Config from "./config";
+import TableCell from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [inputText, setInputText] = useState("");
+    const [data, setData] = useState({ sites: [] });
+    const [isLoading, setIsLoading] = useState(false);
+    const [err, setErr] = useState('');
+    const [errorText, setErrText] = useState('');
+
+    let inputHandler = (e) => {
+        //convert search text to lower case
+        var lowerCase = e.target.value.toLowerCase();
+        setInputText(lowerCase);
+    };
+
+    const handleClick = async () => {
+
+        setIsLoading(true);
+        try {
+            if (inputText.length === 0) {
+                setErrText("Input Cannot be empty!");
+            } else {
+                const { data } = await axios.get(Config.getCommonURL() + '/extracted-url-by-text?text=' + inputText, {
+                    headers: {
+                        Accept: 'application/json',
+                    },
+                });
+                setErrText("");
+                setData(data);
+            }
+
+
+
+        } catch (err) {
+            setErr(err.message);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    return (
+        <div className="main">
+            <h1>Web Crawler</h1>
+
+            <Stack direction="row" spacing={2} >
+                <TextField
+                    id="outlined-basic"
+                    variant="outlined"
+                    label="Search Here"
+                    value={inputText}
+                    onChange={inputHandler}
+                    required
+                    error={inputText.length === 0 ? false : true}
+                    helperText={errorText}
+                />
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleClick}
+                >
+                    Search
+                </Button>
+            </Stack>
+
+            {err && <Alert severity="error">
+                <AlertTitle>Error</AlertTitle>
+                {err}
+            </Alert>}
+
+            {isLoading && <h2>Loading...</h2>}
+
+            {data.sites.map(siteData => {
+
+                return (
+                    <TableRow>
+                        <TableCell>
+                            {siteData.url}
+                        </TableCell>
+                    </TableRow>
+
+                );
+            })}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,7 @@
+class Config {
+    getCommonURL() {
+      return "http://localhost:8080/api/web-crawler/v1";
+    }
+  }
+  export default new Config();
+  
